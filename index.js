059009ddeab8bb74086888c19d106ad7f74b2b6a
Rody Huancas <rodyhuancas.04@gmail.com>
2023-02-05 21:46:20 -0500

    conexión a la bd

---
+++ index.js
@@ -0,0 +1,36 @@
+import express from 'express';
+import router from './routes/index.js';
+import db from './config/db.js';
+
+const app = express();
+
+// conectar la bd
+db.authenticate()
+    .then(() => console.log('bd conectada'))
+    .catch(error => console.log(error))
+
+// definir puerto
+const port = process.env.PORT || 4000;
+
+// habilitar pug
+app.set('view engine', 'pug');
+
+// obtener el año actual
+app.use((req, res, next) => {
+    const year = new Date();
+    res.locals.actualYear = year.getFullYear();
+    res.locals.nombreSitio = 'Agencia de Viajes';
+    // pasar al siguiente midelwere
+    return next();
+});
+
+// definir la carpeta public
+app.use(express.static('public'));
+
+// agregar router
+app.use('/', router);
+
+app.listen(port, () => {
+    console.log(`El servidor está funcionando en el puerto ${port}`);
+})
+
---
+++ routes/index.js
@@ -0,0 +1,18 @@
+import express from "express";
+import {
+    paginaInicio,
+    paginaNosotros,
+    paginaViajes,
+    paginaTestimoniales,
+} from "../controllers/paginasController.js";
+
+const router = express.Router();
+
+router.get("/", paginaInicio);
+
+router.get("/nosotros", paginaNosotros);
+
+router.get("/viajes", paginaViajes);
+router.get("/testimoniales", paginaTestimoniales);
+
+export default router;
