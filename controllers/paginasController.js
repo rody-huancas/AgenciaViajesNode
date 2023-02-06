4468f5e85f6074549c38951a7a115553debfc874
Rody Huancas <rodyhuancas.04@gmail.com>
2023-02-06 12:39:23 -0500

    Finalizado

--- controllers/paginasController.js
+++ controllers/paginasController.js
@@ -2,11 +2,24 @@ import { Viaje } from "../models/Viaje.js";
 import { Testimonial } from "../models/testimoniales.js";
 
 // req -> lo que enviamos, res-> lo que express nos responde
-const paginaInicio = (req, res) => {
-    res.render("inicio", {
-        pagina: "Inicio",
-        clase: "home"
-    });
+const paginaInicio = async (req, res) => {
+    // consultar 3 viajes del modelo viaje
+    const promiseDB = [];
+
+    promiseDB.push(Viaje.findAll({ limit: 3 }));
+    promiseDB.push(Testimonial.findAll({ limit: 3 }));
+    try {
+        const resultado = await Promise.all(promiseDB);
+
+        res.render("inicio", {
+            pagina: "Inicio",
+            clase: "home",
+            viajes: resultado[0],
+            testimoniales: resultado[1]
+        });
+    } catch (error) {
+        console.log(error);
+    }
 };
 
 const paginaNosotros = (req, res) => {
